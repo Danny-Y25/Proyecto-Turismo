213bab240947de2142e15816ee69933bb8c915f7
Danny-Y25 <69018308+Danny-Y25@users.noreply.github.com>
2021-01-19 13:12:39 -0500

    Version19/1/2021
    Actualizacion con el login y registro de usuarios

--- Codigo/GualaquizaTourist/app/src/main/java/com/example/gualaquiza_tourist/MainActivity.kt
+++ Codigo/GualaquizaTourist/app/src/main/java/com/example/gualaquiza_tourist/MainActivity.kt
@@ -2,9 +2,16 @@ package com.example.gualaquiza_tourist
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.*
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import org.w3c.dom.Text
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +25,38 @@ class MainActivity : AppCompatActivity() {
             val miIntent = Intent(this, Registro::class.java)
             startActivity(miIntent)
         }
+        //setup
+        login();
+
+        }
+        private fun login() {
+            title = "Autentificacion"
+            val login : Button = findViewById(R.id.btnlogin)
+            val contra =  findViewById<EditText>(R.id.editTextTextPassword)
+            val mail = findViewById<EditText>(R.id.editTextTextPersonName)
+
+            login.setOnClickListener {
+                if (mail.text.isNotEmpty() && contra.text.isNotEmpty()) {
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(mail.text.toString(), contra.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            login.setOnClickListener{
+                                val mostrar = Intent(this,Menu::class.java)
+                                startActivity(mostrar)
+                            }
+                        }else{
+                            alert()
+                        }
+                    }
+                }
+            }
+        }
+        private fun alert(){
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Error")
+            builder.setMessage("Se ha producido un error autentificando al usuario")
+            builder.setPositiveButton("Aceptar",null)
+            val dialog: AlertDialog=builder.create()
+            dialog.show()
 
         }
     }
--- Codigo/GualaquizaTourist/app/src/main/java/com/example/gualaquiza_tourist/Registro.kt
+++ Codigo/GualaquizaTourist/app/src/main/java/com/example/gualaquiza_tourist/Registro.kt
@@ -1,12 +1,47 @@
 package com.example.gualaquiza_tourist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class Registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        login()
+    }
+    private fun login() {
+        title = "Autentificacion"
+        val register : Button = findViewById(R.id.btnregis)
+        val contra =  findViewById<EditText>(R.id.editTextTextPersonName2)
+        val mail = findViewById<EditText>(R.id.editTextTextPersonName7)
+
+        register.setOnClickListener {
+            if (mail.text.isNotEmpty() && contra.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(mail.text.toString(), contra.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        register.setOnClickListener{
+                            val mostrar = Intent(this,MainActivity::class.java)
+                            startActivity(mostrar)
+                        }
+                    }else{
+                        alert()
+                    }
+                }
+            }
+        }
+    }
+    private fun alert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autentificando al usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog =builder.create()
+        dialog.show()
 
     }
 }
